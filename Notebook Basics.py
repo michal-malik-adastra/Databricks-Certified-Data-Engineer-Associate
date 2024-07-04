4760cabaa4fdfef5e2ca460104c3ded089778243
michal-malik-adastra <michal-malik-adastra>
2024-07-04 08:14:44 +0000

    ?

---
+++ Notebook Basics.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # A
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## a
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## b
+
+# COMMAND ----------
+
+
